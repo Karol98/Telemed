2ddd32703a3c45bf25f0c160c2450bf3571055fc
Karol <Kskwierawski12@gmail.com>
2021-01-24 16:17:40 +0100

    added DoctorFinder

--- src/components/DoctorFinder.js
+++ src/components/DoctorFinder.js
@@ -1,41 +1,52 @@
 import React, {useState} from "react";
 import Footer from "./Footer";
 import {Form} from "react-bootstrap";
-import firebase from "firebase";
-
-// Nie działa przez CORS :(
+import NaviBar from "./NaviBar";
 
 export default function DoctorFinder() {
-    const [latitude, SetLatitude] = useState("54.33573817998878");
-    const [longitude, SetLongitude] = useState("18.198659771185657");
+    const [hasLocationPermission, SetLocationPermission] = useState(false);
+    const [Latitude, SetLatitude] = useState();
+    const [Longitude, SetLongitude] = useState();
     const [list, SetList] = useState();
 
-    const handleSearch = async (e) => {
-        e.preventDefault();
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
-        const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
-        const location = `location=${latitude},${longitude}`;
-        const radius = '&radius=2000';
-        const type = '&keyword=restaurant';
-        const key = '&key=AIzaSyCF3BBVNrwHWDCWqjyQfqXGTZzrA9rjpnw';
-        const SearchUrl = proxy + url + location + radius + type + key;
-        await myRespone(SearchUrl);
-        console.log(list);
+
+    function getLocation() {
+        return new Promise(resolve =>
+            navigator.geolocation.getCurrentPosition(function (position) {
+                resolve(position.coords);
+            }));
     }
 
-    function myRespone(SearchUrl) {
-        fetch(SearchUrl).then(response => response.json()).then(result => {
-            SetList({list: result})
-        })
+    async function handleSearch()  {
+        let locations = await getLocation();
+        console.log(locations);
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+        const location = `location=${locations.latitude},${locations.longitude}`;
+        const radius = '&radius=2000';
+        const type = '&keyword=doctor';
+        const key = '&key=AIzaSyCF3BBVNrwHWDCWqjyQfqXGTZzrA9rjpnw';
+        const SearchUrl = proxy + url + location + radius + type + key;
+        let x = await myRespone(SearchUrl);
+        console.log(x.results);
+    }(handleSearch())
+
+    async function myRespone(SearchUrl) {
+        try {
+            console.log(SearchUrl);
+            let res = await fetch(SearchUrl);
+            return res.json();
+        } catch (err) {
+            console.error(err)
+        }
     }
 
     return (
         <div>
+            <NaviBar/>
             <br/>
             <br/>
-            <Form onSubmit={handleSearch}>
-                <button type="submit" className="btn-primary">Szukaj Lekarzy</button>
-            </Form>
+            <br/>
             <Footer/>
         </div>
     )
